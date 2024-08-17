efe9ae3d71c5109c62614c478c84173f0a1671a2
Benrise <benrise_x@mail.ru>
2024-08-17 14:45:43 +0300

    Add base tracer

--- services/auth/main.py
+++ services/auth/main.py
@@ -3,7 +3,7 @@ import uvicorn
 
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, status
 from fastapi.responses import JSONResponse
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
@@ -20,6 +20,13 @@ from core.config import settings
 from core.logger import LOGGING
 from db import redis
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider        
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import Resource, SERVICE_NAME
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -28,6 +35,7 @@ async def lifespan(app: FastAPI):
     if settings.debug:
         from db.postgres import create_database
         await create_database()
+    configure_tracer()
     yield
     await redis.redis.close()
 
@@ -42,18 +50,38 @@ app = FastAPI(
 )
 
 
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "auth-service"})
+    ))
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name='jaeger',
+                agent_port=6831,
+            )
+        )
+    )
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
 @app.middleware('http')
 async def before_request(request: Request, call_next):
-    return await call_next(request)
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+    return response
 
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(_: Request, exc: AuthJWTException):
     return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
 
+
 app.include_router(users.router, prefix='/auth/api/v1/users', tags=['users'], dependencies=[Depends(get_current_user_global)])
 app.include_router(roles.router, prefix='/auth/api/v1/roles', tags=['roles'], dependencies=[Depends(get_current_user_global)])
 
+FastAPIInstrumentor.instrument_app(app)
 
 if __name__ == '__main__':
     uvicorn.run(
